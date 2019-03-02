71ca975824caa9f8a774ab6654309b8b1e27b6d7
Elena Zhurbytska <Lnchkeoa@gmail.com>
2019-03-02 21:23:09 +0200

    26.02.2019 classwork references

--- Classworks/19.02.2019/19.02.2019/Source.cpp
+++ Classworks/19.02.2019/19.02.2019/Source.cpp
@@ -1,148 +1,71 @@
 ﻿////// POINTERS ////
-//# include <iostream>
-//using namespace std;
-//
-//void Func(int *pA, int *pB)
-//{
-//	cout << "Func *pA value  " << *pA << "  a adress " << pA << endl;
-//	cout << "Func *pB value  " << *pB << "  a adress " << pB << endl;
-//	
-//	//створили тимчасову змінну для обміну значень змінних місцями// 
-//	int temp = *pA;
-//	*pA = *pB;
-//	*pB = temp;
-//	cout << "=================AFTER================" << endl;
-//	//поміняли місцями значення, не адреси
-//	cout << "Func *pA value  " << *pA << "  a adress " << pA << endl;
-//	cout << "Func *pB value  " << *pB << "  a adress " << pB << endl;
-//}
-//
-//void Foo(int a, int b)
-//{
-//	cout << "Foo a value  " << a << "  a adress " << &a << endl;
-//	cout << "Foo a value  " << b << "  a adress " << &b << endl;
-//
-//	
-//
-//}
-//
-//int main()
-//{
-//	////вказівники
-//	//////int a = 10;
-//
-//	//////cout << "a value =  " << a << " a adress = " << &a << endl;
-//	//////int *pA = &a;
-//
-//	//////cout << "*pA value =  " << a << " pA adress = " << *pA << endl;
-//
-//	//////cout << "====================================>" << endl;
-//
-//	//////int *pointer = &a;//створили новий вказівник і поклали в нього теж адресу на змінну а
-//
-//
-//	//створимо дві змінних  та поміняємо їх значення місцями
-//
-//	int a = 10, b = 20;
-//
-//	cout << "a value  " << a << "  a adress " << &a << endl;
-//	cout << "a value  " << b << "  a adress " << &b << endl;
-//	cout << "=================================" << endl;
-//
-//	Foo(a, b);
-//	int *pA = &a;
-//	int *pB = &b;
-//
-//	Func(pA, pB);////передаємо адреси в пам'яті
-//
-//	cout << "----------------- RESULT --------------------" << endl;
-//	cout << "a value  " << a << "  a adress " << &a << endl;
-//	cout << "a value  " << b << "  a adress " << &b << endl;
-//	cout << "=================================" << endl;
-//	
-//
-//	system("pause");
-//	return 0;
-//}
+# include <iostream>
+using namespace std;
 
-
-
-
-/*Дано три числа.Оголосити вказівники на ці числа.
-	Отримати добуток трьох заданих чисел, середє арифметичне, 
-	найменше з них, користуючись непрямим доступом до чисел (через вказівники).
+void Func(int *pA, int *pB)
+{
+	cout << "Func *pA value  " << *pA << "  a adress " << pA << endl;
+	cout << "Func *pB value  " << *pB << "  a adress " << pB << endl;
 	
-	Виконати задачу 1 з використанням void вказівників.  
-	*/
+	//створили тимчасову змінну для обміну значень змінних місцями// 
+	int temp = *pA;
+	*pA = *pB;
+	*pB = temp;
+	cout << "=================AFTER================" << endl;
+	//поміняли місцями значення, не адреси
+	cout << "Func *pA value  " << *pA << "  a adress " << pA << endl;
+	cout << "Func *pB value  " << *pB << "  a adress " << pB << endl;
+}
+
+void Foo(int a, int b)
+{
+	cout << "Foo a value  " << a << "  a adress " << &a << endl;
+	cout << "Foo a value  " << b << "  a adress " << &b << endl;
+
+	
+
+}
+
+int main()
+{
+	////вказівники
+	//////int a = 10;
+
+	//////cout << "a value =  " << a << " a adress = " << &a << endl;
+	//////int *pA = &a;
+
+	//////cout << "*pA value =  " << a << " pA adress = " << *pA << endl;
+
+	//////cout << "====================================>" << endl;
+
+	//////int *pointer = &a;//створили новий вказівник і поклали в нього теж адресу на змінну а
 
 
-//# include <iostream>
-//using namespace std;
-//
-//
-//void Dob(int *pA, int *pB, int *pC, int*pDob);
-//void Average(int *pA, int *pB, int *pC, int *pSAryph);
-//void Minim(int *pA, int *pB, int *pC, int *pMin);
-//
-//
-//
-//
-//int main()
-//{
-//	int a = 5;
-//	int b = 10;
-//	int c = 20;
-//
-//	int *pA = &a;
-//	int *pB = &b;
-//	int *pC = &c;
-//
-//	int dob = 0;
-//	int SAryph = 0;
-//	int Min = 0;
-//
-//	int *pDob = &dob;
-//	int *pSAryph = &SAryph;
-//	int *pMin=&Min;
-//
-//	Dob(pA, pB, pC, pDob);
-//	Average(pA, pB, pC, pSAryph);
-//	Minim(pA, pB, pC, pMin);
-//
-//	cout << "Dobutok a*b*c = " << *pDob << endl;
-//	cout << "Seredne aryfmetychne = " << *pSAryph << endl;
-//	cout << "Minimalne = " << *pMin << endl;
-//
-//	system("pause");
-//	return 0;
-//}
-//void Dob(int *pA, int *pB, int *pC, int*pDob)
-//{
-//	int dob = 0;
-//	*pDob = (*pA) * (*pB) * (*pC);	
-//}
-//
-//void Average(int *pA, int *pB, int *pC, int *pSAryph)
-//{	
-//	*pSAryph = ((*pA) + (*pB) + (*pC))/3;	
-//}
-//
-//void Minim(int *pA, int *pB, int *pC, int *pMin)
-//{
-//	
-//	if(*pA<*pB && *pA<*pC)
-//	{
-//		*pMin = *pA;
-//	}
-//	else if(*pB < *pA && *pB < *pC)
-//	{
-//		*pMin = *pB;
-//	}
-//	else if (*pC < *pA && *pC < *pB)
-//	{
-//		*pMin = *pC;
-//	}	
-//}
+	//створимо дві змінних  та поміняємо їх значення місцями
+
+	int a = 10, b = 20;
+
+	cout << "a value  " << a << "  a adress " << &a << endl;
+	cout << "a value  " << b << "  a adress " << &b << endl;
+	cout << "=================================" << endl;
+
+	Foo(a, b);
+	int *pA = &a;
+	int *pB = &b;
+
+	Func(pA, pB);////передаємо адреси в пам'яті
+
+	cout << "----------------- RESULT --------------------" << endl;
+	cout << "a value  " << a << "  a adress " << &a << endl;
+	cout << "a value  " << b << "  a adress " << &b << endl;
+	cout << "=================================" << endl;
+	
+
+	system("pause");
+	return 0;
+}
+
+
 
 
 /*Дано три числа.Оголосити вказівники на ці числа.
@@ -150,143 +73,162 @@
 	найменше з них, користуючись непрямим доступом до чисел (через вказівники).
 
 	Виконати задачу 1 з використанням void вказівників.
-
-	Зробити її шаблонною
 	*/
 
-//# include <iostream>
-//using namespace std;
-//
-//template <typename T>
-//void Dob(T *pA, T *pB, T *pC, T *pDob);
-//
-//template <typename T>
-//void Average(T *pA, T *pB, T *pC, T *pSAryph);
-//
-//template <typename T>
-//void Minim(T *pA, T *pB, T *pC, T *pMin);
-//
-//
-//
-//
-//int main()
-//{
-//	double a = 5;
-//	double b = 10;
-//	double c = 20;
-//
-//	double *pA = &a;
-//	double *pB = &b;
-//	double *pC = &c;
-//
-//	double dob = 0;
-//	double SAryph = 0;
-//	double Min = 0;
-//
-//	double *pDob = &dob;
-//	double *pSAryph = &SAryph;
-//	double *pMin = &Min;
-//
-//	Dob(pA, pB, pC, pDob);
-//	Average(pA, pB, pC, pSAryph);
-//	Minim(pA, pB, pC, pMin);
-//
-//	cout << "Dobutok a*b*c = " << *pDob << endl;
-//	cout << "Seredne aryfmetychne = " << *pSAryph << endl;
-//	cout << "Minimalne = " << *pMin << endl;
-//
-//	system("pause");
-//	return 0;
-//}
-//
-//template <typename T>
-//void Dob(T *pA, T *pB, T *pC, T *pDob)
-//{
-//	int dob = 0;
-//	*pDob = (*pA) * (*pB) * (*pC);
-//}
-//
-//template <typename T>
-//void Average(T *pA, T *pB, T *pC, T *pSAryph)
-//{
-//	*pSAryph = ((*pA) + (*pB) + (*pC)) / 3;
-//}
-//
-//template <typename T>
-//void Minim(T *pA, T *pB, T *pC, T *pMin)
-//{
-//
-//	if (*pA < *pB && *pA < *pC)
-//	{
-//		*pMin = *pA;
-//	}
-//	else if (*pB < *pA && *pB < *pC)
-//	{
-//		*pMin = *pB;
-//	}
-//	else if (*pC < *pA && *pC < *pB)
-//	{
-//		*pMin = *pC;
-//	}
-//}
 
-
-/*Написать  калькулятор, пользуясь только указателями.*/
-
-# include <iostream>
-using namespace std;
-
-
-
-
-int main()
-{
-	cout << "/////////// CALCULATOR ////////////" << endl;
-	cout << "choose an operation:  [1] - ""+""addition \n [2] - ""-""substruct; \n [3] - ""*"" multiplication; ""/"" deviding"  << endl;
-	cout << "Enter first number" << endl;
-	 int a = 0;
-	 cin >> a;
-	 cout << "Enter second number" << endl;
-	 int b = 0;
-	 cin >> b;
-
+	# include <iostream>
+	using namespace std;
 	
-
-	 
-
 	
-
-	system("pause");
-	return 0;
-}
-
-template <typename T>
-void Dob(T *pA, T *pB, T *pC, T *pDob)
-{
-	int dob = 0;
-	*pDob = (*pA) * (*pB) * (*pC);
-}
-
-template <typename T>
-void Average(T *pA, T *pB, T *pC, T *pSAryph)
-{
-	*pSAryph = ((*pA) + (*pB) + (*pC)) / 3;
-}
-
-template <typename T>
-void Minim(T *pA, T *pB, T *pC, T *pMin)
-{
-
-	if (*pA < *pB && *pA < *pC)
+	void Dob(int *pA, int *pB, int *pC, int*pDob);
+	void Average(int *pA, int *pB, int *pC, int *pSAryph);
+	void Minim(int *pA, int *pB, int *pC, int *pMin);
+	
+	
+	
+	
+	int main()
 	{
-		*pMin = *pA;
+		int a = 5;
+		int b = 10;
+		int c = 20;
+	
+		int *pA = &a;
+		int *pB = &b;
+		int *pC = &c;
+	
+		int dob = 0;
+		int SAryph = 0;
+		int Min = 0;
+	
+		int *pDob = &dob;
+		int *pSAryph = &SAryph;
+		int *pMin=&Min;
+	
+		Dob(pA, pB, pC, pDob);
+		Average(pA, pB, pC, pSAryph);
+		Minim(pA, pB, pC, pMin);
+	
+		cout << "Dobutok a*b*c = " << *pDob << endl;
+		cout << "Seredne aryfmetychne = " << *pSAryph << endl;
+		cout << "Minimalne = " << *pMin << endl;
+	
+		system("pause");
+		return 0;
 	}
-	else if (*pB < *pA && *pB < *pC)
+	void Dob(int *pA, int *pB, int *pC, int*pDob)
 	{
-		*pMin = *pB;
+		int dob = 0;
+		*pDob = (*pA) * (*pB) * (*pC);	
 	}
-	else if (*pC < *pA && *pC < *pB)
+	
+	void Average(int *pA, int *pB, int *pC, int *pSAryph)
+	{	
+		*pSAryph = ((*pA) + (*pB) + (*pC))/3;	
+	}
+	
+	void Minim(int *pA, int *pB, int *pC, int *pMin)
 	{
-		*pMin = *pC;
+		
+		if(*pA<*pB && *pA<*pC)
+		{
+			*pMin = *pA;
+		}
+		else if(*pB < *pA && *pB < *pC)
+		{
+			*pMin = *pB;
+		}
+		else if (*pC < *pA && *pC < *pB)
+		{
+			*pMin = *pC;
+		}	
 	}
-}
+
+
+	/*Дано три числа.Оголосити вказівники на ці числа.
+		Отримати добуток трьох заданих чисел, середє арифметичне,
+		найменше з них, користуючись непрямим доступом до чисел (через вказівники).
+
+		Виконати задачу 1 з використанням void вказівників.
+
+		Зробити її шаблонною
+		*/
+
+		# include <iostream>
+		using namespace std;
+		
+		template <typename T>
+		void Dob(T *pA, T *pB, T *pC, T *pDob);
+		
+		template <typename T>
+		void Average(T *pA, T *pB, T *pC, T *pSAryph);
+		
+		template <typename T>
+		void Minim(T *pA, T *pB, T *pC, T *pMin);
+		
+		
+		
+		
+		int main()
+		{
+			double a = 5;
+			double b = 10;
+			double c = 20;
+		
+			double *pA = &a;
+			double *pB = &b;
+			double *pC = &c;
+		
+			double dob = 0;
+			double SAryph = 0;
+			double Min = 0;
+		
+			double *pDob = &dob;
+			double *pSAryph = &SAryph;
+			double *pMin = &Min;
+		
+			Dob(pA, pB, pC, pDob);
+			Average(pA, pB, pC, pSAryph);
+			Minim(pA, pB, pC, pMin);
+		
+			cout << "Dobutok a*b*c = " << *pDob << endl;
+			cout << "Seredne aryfmetychne = " << *pSAryph << endl;
+			cout << "Minimalne = " << *pMin << endl;
+		
+			system("pause");
+			return 0;
+		}
+		
+		template <typename T>
+		void Dob(T *pA, T *pB, T *pC, T *pDob)
+		{
+			int dob = 0;
+			*pDob = (*pA) * (*pB) * (*pC);
+		}
+		
+		template <typename T>
+		void Average(T *pA, T *pB, T *pC, T *pSAryph)
+		{
+			*pSAryph = ((*pA) + (*pB) + (*pC)) / 3;
+		}
+		
+		template <typename T>
+		void Minim(T *pA, T *pB, T *pC, T *pMin)
+		{
+		
+			if (*pA < *pB && *pA < *pC)
+			{
+				*pMin = *pA;
+			}
+			else if (*pB < *pA && *pB < *pC)
+			{
+				*pMin = *pB;
+			}
+			else if (*pC < *pA && *pC < *pB)
+			{
+				*pMin = *pC;
+			}
+		}
+
+
+		
---
+++ Classworks/26.02.2019C/26.02.2019C/Source.cpp
@@ -0,0 +1,260 @@
+////////////////////////////////// CLASSWORK /////////////////////////////////////
+////////////////////////////////// REFERENCE /////////////////////////////////////
+
+//#include <iostream>
+//#include <ctime>
+//using namespace std;
+//
+//int main()
+//{
+//	int a = 10;
+//	cout << "a = " << a << endl;
+//	cout << "adress a = " << &a << endl;
+//
+//									int *pA = &a;
+//	cout << "Pointer a" << *pA << "\tAdress pointer a	" << pA << endl;
+//	/*Зараз на одну і ту саму адресу ссилаються і вказівник і змінна*/
+//	//Test (int a) - значення змінної а буде те саме, але під час передачі змінної а в функцію, в неї змінюється адреса
+//	//якщо ж ми в функцію передаємо вказівник на змінну, то адреса буде та сама!!!
+//	//ФЛЬТЕРНАТИВА ВКАЗІВНИКУ
+//									int &RA = a;
+//	//REFERENCE
+//									cout << "Reference " << RA <<"	Adress reference	"<< &RA << endl;
+////Та сама адреса, бо ми посилаємось на ту саму змінну
+//
+//	system("pause");
+//	return 0;
+//}
+
+//////example.
+//////# include <iostream>
+//////
+//////
+//////using namespace std;
+//////
+//////void FVAR(int a) {
+//////	cout << "FVAR BEFORE a  value: " << a << " : a addr : " << &a << endl;
+//////	a += 100;
+//////	cout << "FVAR AFTER a  value: " << a << " : a addr : " << &a << endl;
+//////}
+//////void FREF(int &a) {
+//////	cout << "FREF BEFORE a  value: " << a << " : a addr : " << &a << endl;
+//////	a += 300;
+//////	cout << "FREF AFTER a  value: " << a << " : a addr : " << &a << endl;
+//////}
+//////
+//////void FPTR(int *a) {
+//////	cout << "FPTR BEFORE a  value: " << *a << " : a addr : " << a << endl;
+//////	*a += 500;
+//////	cout << "FPTR AFTER a  value: " << *a << " : a addr : " << a << endl;
+//////}
+//////
+//////
+//////int main() {
+//////
+//////	int a = 10;
+//////
+//////	cout << "main a  value: " << a << "  : a addr : " << &a << endl;
+//////	//int *pA = &a;
+//////	//cout << "*pA  value: " << *pA << " : *pA addr : " << pA << endl;
+//////	//cout << "========================Before Ra==================>" << endl;
+//////
+//////	//int &Ra = a;
+//////
+//////	//cout << "a  value: " << a << " : a addr : " << &a << endl;
+//////	//cout << "&Ra  value: " << Ra << " : &Ra addr : " << &Ra << endl;
+//////	cout << "========================After Ra==================>" << endl;
+//////
+//////	FVAR(a);
+//////	//FREF(Ra);
+//////	FREF(a); //FREF( Ra );
+//////	FPTR(&a);
+//////
+//////	cout << "main a  value: " << a << "  : a addr : " << &a << endl;
+//////
+//////	system("pause");
+//////	return 0;
+//////}
+
+
+/*		Дано три числа.Оголосити посилання на ці числа. Отримати добуток
+		трьох заданих чисел, середє арифметичне, найменше з них, користуючись
+		непрямим доступом до чисел(через посилання).*/
+		//#include <iostream>
+		//#include <ctime>
+		//using namespace std;
+		//void Dob(int &Ra, int &Rb, int &Rc);
+		//void Average(int &Ra, int &Rb, int &Rc);
+		//void Min(int &Ra, int &Rb, int &Rc);
+		//
+		//int main()
+		//{
+		//	int a = 5, b = 7, c = 10;
+		//	int &Ra = a, &Rb = b, &Rc = c;
+		//	Dob(Ra, Rb, Rc);
+		//	Average(Ra, Rb, Rc);
+		//	Min(Ra, Rb, Rc);
+		//	system("pause");
+		//	return 0;
+		//}
+		//
+		//void Dob(int &Ra, int &Rb, int &Rc) 
+		//{
+		//	cout << "Dobutok = " << Ra * Rb*Rc << endl;
+		//}
+		//
+		//void Average(int &Ra, int &Rb, int &Rc)
+		//{
+		//	cout << "Seredne Arufmetychne = " << (Ra + Rb + Rc) / 3 << endl;
+		//}
+		//
+		//void Min(int &Ra, int &Rb, int &Rc)
+		//{
+		//	
+		//	if(Ra<Rb && Ra<Rc)
+		//	{
+		//		cout << "Minimalne = " << Ra << endl;
+		//	}
+		//	else if(Rb < Ra && Rb < Rc)
+		//	{
+		//		cout << "Minimalne = " << Rb << endl;
+		//	}
+		//	else if (Rc < Ra && Rc < Rb)
+		//	{
+		//		cout << "Minimalne = " << Rc << endl;
+		//	}
+		//} 
+
+
+
+
+/*Написати функцію, яка отримує одновимірний масив і повертає ссилку на
+найменший елемент масиву.За допомогою цієї функції вивести на екран найменший
+елемент та подвоїти найменший елемент масиву.*/
+
+//////#include <iostream>
+//////#include <ctime>
+//////using namespace std;
+
+//////void Fill(int arr[], int SIZE);
+//////void Print(int arr[], int SIZE);
+//////void MinArr(int arr[], int SIZE, int &Rmin);
+//////
+//////int main()
+//////{
+//////	srand(unsigned(time(NULL)));
+//////	const int SIZE = 5;
+//////	int arr[SIZE] = {};
+//////	int min = 111110;
+//////	int &Rmin = min;
+//////
+//////	Fill(arr, SIZE);
+//////	Print(arr, SIZE);
+//////	MinArr(arr, SIZE, Rmin);
+//////	cout << "Min element = " << Rmin << endl;
+//////	cout << "Min element adress = " << &Rmin << endl;
+//////	cout << "Min element *2 = " << Rmin * 2 << endl;
+//////
+//////
+//////	system("pause");
+//////	return 0;
+//////}
+//////
+//////void Fill(int arr[], int SIZE)
+//////{
+//////	for (int i = 0; i < SIZE; i++)
+//////	{
+//////		arr[i] = rand() % 10 + 1;
+//////	}
+//////}
+//////
+//////void Print(int arr[], int SIZE)
+//////{
+//////	for (int i = 0; i < SIZE; i++)
+//////	{
+//////		cout << arr[i] << "	";
+//////	}cout << endl;
+//////
+//////}
+//////
+//////void MinArr(int arr[], int SIZE, int &Rmin)
+//////{
+//////
+//////	for (int i = 0; i < SIZE; i++)
+//////	{
+//////
+//////		if (arr[i] < Rmin)
+//////		{
+//////			Rmin = arr[i];
+//////		}
+//////	}
+//////}
+
+/*Написати функцію, яка замінює всі від’ємні елементи переданого масиву 
+нулями. Примітка! Функція повинна повертати ссилку на відємний елемент*/
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+void Fill(int arr[], int SIZE, int &Ra);
+void Print(int arr[], int SIZE);
+void Zero(int arr[], int SIZE, int &Ri);
+
+
+int main()
+{
+	srand(unsigned(time(NULL)));
+	const int SIZE = 5;
+	int arr[SIZE] = {};
+	int a = 0;
+	int &Ra = a;	
+	int i = 0;
+	int &Ri = i;
+	
+	Fill(arr, SIZE, Ra);
+	cout << "Array before changes" << endl;
+	Print(arr, SIZE);
+	Zero(arr, SIZE, i);
+
+	cout << "Array after changes/ All negative elements were changed by zeroes" << endl;
+	Print(arr, SIZE);
+	
+	system("pause");
+	return 0;
+}
+
+void Fill(int arr[], int SIZE, int &Ra)
+{
+	cout << "Enter number from - 10 to 10;" << endl;
+	for (int i = 0; i < SIZE; i++)
+	{
+		cin >> Ra;
+		arr[i] = Ra;
+	}
+}
+
+void Print(int arr[], int SIZE)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "	";
+	}
+	cout << endl;
+
+}
+
+void Zero(int arr[], int SIZE, int &Ri)
+{
+	for (int Ri = 0; Ri < SIZE; Ri++)
+
+	{
+		if (arr[Ri] < 0)
+		{			
+			cout << "Negative element " << Ri << " = " << arr[Ri] << endl;
+			arr[Ri] = 0;
+		}
+	}
+}
+
