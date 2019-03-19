aeb360b1e86dde5b0d40eb9ed7433af69f7378d6
Elena Zhurbytska <Lnchkeoa@gmail.com>
2019-03-19 11:34:48 +0200

    14.03.2019H

---
+++ Homeworks/14.03.2019H/14.03.2019H/Source.cpp
@@ -0,0 +1,19 @@
+/////14.03.2019H////
+
+//# include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	char strArr[255];
+//	cout << "Enter strArr" << endl;
+//	cin >> strArr;
+//	for (int i = 0; i < strlen(strArr); i++)
+//	{
+//		cout << "Text: " << strArr[i] << " CharCode = " << int(strArr[i]) << "\t";
+//	}
+//	cout << endl;
+//
+//	system("pause");
+//	return 0;
+//}
