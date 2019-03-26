edfd4199505d61a003d4dcdd693f46011ec54155
Elena Zhurbytska <Lnchkeoa@gmail.com>
2019-03-26 20:12:18 +0200

    19.03.2019H

--- Homeworks/19.03.2019H/19.03.2019H/Source.cpp
+++ Homeworks/19.03.2019H/19.03.2019H/Source.cpp
@@ -5,80 +5,90 @@
 пробільних символів (whitespaces), голосних, приголосних, знаків пунктуації. 
 Введення передбачається англомовним. 
 */
-
+ 
 # include <iostream>
-# include <string> //switch on class string
+# include <string>		//switch on class string
 using namespace std;
-int counterVovel = 0; // golosni zvuky
-int counterConsonant = 0; //prygolosni zvuky
-int counterNum = 0; //counter of numbers
-int counterSp = 0; //counter of spaces
-int counterAllSym = 0; // counter of another symbols
 
-void Foo(char *str);
+//Function whith parametres array str, pointers for the counters
+void SignsOfSound(char *str, int *pcntrVovel, int *pcntrConsonant, int *pcntrSp, int *pcntrComa);
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+	int cntrVovel = 0;	 //golosni zvuky
+	int cntrConsonant = 0;	 //prygolosni zvuky
+	int cntrSp = 0;		//counter of spaces
+	int cntrComa = 0;	// counter of punctuation
+
+	int *pcntrVovel = &cntrVovel;
+	int *pcntrConsonant = &cntrConsonant;
+	int *pcntrSp = &cntrSp;
+	int *pcntrComa = &cntrComa;
+
 	char str[255];
-	cout << "Enter text (any symbols): " << endl;
+	cout << "Enter text - any symbols, but instead witespace enter ""_"" " << endl;
 	cin >> str;
-	cout << "Common number of elemets is:  ";
-	cout << strlen(str) << endl;
 	
-	Foo(str);
+	//This function counts number of whitespaces, vovel, consonant sounds and punctuation signs
+	
+	SignsOfSound(str, pcntrVovel, pcntrConsonant, pcntrSp, pcntrComa);
 
-	cout << "Amount of vovel sounds:	" << counterVovel << endl;
-	cout << "Amount of consonant sounds:	" << counterConsonant << endl;
-	cout << "Amount of numbers:	" << counterNum << endl;
-	cout << "Amount of whitespaces:	" << counterSp << endl;
+	cout << "Vovel sounds:	" << *pcntrVovel << endl;
+	cout << "Consonant sounds:	" << *pcntrConsonant << endl;
+	cout << "Punctuation signs:	" << *pcntrComa << endl;
+	cout << "Whitespaces:	" << *pcntrSp << endl;
 
-
+	char rem[2]; //Symbol need to remote
+	cout << "What symbol need to remove?" << endl;
+	cin >> rem;
+	
 	system("pause");
 	return 0;
 }
 
-void Foo(char *str) {
-
+void SignsOfSound(char *str, int *pcntrVovel, int *pcntrConsonant, int *pcntrSp, int *pcntrComa) {
+	
+	
 	for (int i = 0; i < strlen(str); i++)
 	{
-
 		if (str[i] >= 65 && str[i] <= 90 || str[i] >= 97 && str[i] <= 122) {
-			/*cout << char(str[i]) << " ";*/
 			
-			/*cout << "Latin " << endl;*/
-			
-			if (str[i] == 65 || str[i] == 69 || str[i] == 73 || str[i] == 79 || str[i] == 85 || str[i] == 89)
+			if (str[i] == 65 || str[i] == 69 || str[i] == 73 || str[i] == 79 || str[i] == 85 || str[i] == 97 || str[i] == 101 || str[i] == 105 || str[i] == 111 || str[i] == 117)
 			{
-				/*cout << "vovel sound (golosnyj)" << endl;*/
-				counterVovel++;
+				*pcntrVovel++;				
 			}
-			
-			else if (str[i] == 97 || str[i] == 101 || str[i] == 105 || str[i] == 111 || str[i] == 117 || str[i] == 121)
-			{
-				/*cout << "vovel sound (golosnyj)" << endl;*/
-				counterVovel++;
-			}
-
-
 			else
-			{
-				/*cout << "consonant sound (prygolosnyj)" << endl;*/
-				counterConsonant++;
+			{				
+				*pcntrConsonant++;
 			}
 		}
 						
-		else if (str[i] >= 48 && str[i] <= 57) {
-			/*cout << char(str[i]) << " ";
-			cout << "Number " << endl;*/
-			counterNum++;
+		else if (str[i] == 95) {
+			
+			*pcntrSp++;
 		} 		
 		
-		else {
-			/*cout << char(str[i]) << " ";
-			cout << "Technical symbol " << endl;*/
-			counterAllSym++;
-		}
-		cout << endl;
-	}
+		else if (str[i] == 33 || str[i] == 44){
+			*pcntrComa++;
+		}		
+	}	
 }
+
+
+void RemSymb(char *str, char rem) {
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (str[i] == rem) {
+			for (int j = 0; j < strlen(str); j++)
+			{
+
+			}
+		}
+	}
+	
+
+}
+
+
+
+
