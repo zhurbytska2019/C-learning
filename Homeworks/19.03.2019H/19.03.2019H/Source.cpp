c00057ef592fb55dfd4355dcab6638c7f1aaddbb
Elena Zhurbytska <Lnchkeoa@gmail.com>
2019-03-27 16:50:10 +0200

    task2 19.03.2019Homework

--- Homeworks/19.03.2019H/19.03.2019H/Source.cpp
+++ Homeworks/19.03.2019H/19.03.2019H/Source.cpp
@@ -1,94 +1,93 @@
-///===>>> 19.03.2019H <<<===///
 
-/*1) Дано рядок. Видалити із рядка заданий символ. Результат розмістити у новому рядку. 
-2) Розробити програму, яка зчитує з екрану рядок, а потім видає статистику: кількість 
-пробільних символів (whitespaces), голосних, приголосних, знаків пунктуації. 
-Введення передбачається англомовним. 
-*/
- 
+							///===>>> 19.03.2019H <<<===///
+							////===>>> TASK 1 <<<===////
+/*1) Дано рядок. Видалити із рядка заданий символ. Результат розмістити у новому рядку.
+1)Dano riadok. Vydalyty iz riadka zadanyj symvol. Rezultat rozmistyty u novomu riadku.*/
+////===>>> TASK 1 <<<===////
 # include <iostream>
 # include <string>		//switch on class string
 using namespace std;
 
-//Function whith parametres array str, pointers for the counters
-void SignsOfSound(char *str, int *pcntrVovel, int *pcntrConsonant, int *pcntrSp, int *pcntrComa);
 
 int main()
 {
-	int cntrVovel = 0;	 //golosni zvuky
-	int cntrConsonant = 0;	 //prygolosni zvuky
-	int cntrSp = 0;		//counter of spaces
-	int cntrComa = 0;	// counter of punctuation
-
-	int *pcntrVovel = &cntrVovel;
-	int *pcntrConsonant = &cntrConsonant;
-	int *pcntrSp = &cntrSp;
-	int *pcntrComa = &cntrComa;
-
-	char str[255];
-	cout << "Enter text - any symbols, but instead witespace enter ""_"" " << endl;
-	cin >> str;
-	
-	//This function counts number of whitespaces, vovel, consonant sounds and punctuation signs
-	
-	SignsOfSound(str, pcntrVovel, pcntrConsonant, pcntrSp, pcntrComa);
-
-	cout << "Vovel sounds:	" << *pcntrVovel << endl;
-	cout << "Consonant sounds:	" << *pcntrConsonant << endl;
-	cout << "Punctuation signs:	" << *pcntrComa << endl;
-	cout << "Whitespaces:	" << *pcntrSp << endl;
-
-	char rem[2]; //Symbol need to remote
-	cout << "What symbol need to remove?" << endl;
-	cin >> rem;
 	
 	system("pause");
 	return 0;
 }
 
-void SignsOfSound(char *str, int *pcntrVovel, int *pcntrConsonant, int *pcntrSp, int *pcntrComa) {
-	
-	
-	for (int i = 0; i < strlen(str); i++)
-	{
-		if (str[i] >= 65 && str[i] <= 90 || str[i] >= 97 && str[i] <= 122) {
-			
-			if (str[i] == 65 || str[i] == 69 || str[i] == 73 || str[i] == 79 || str[i] == 85 || str[i] == 97 || str[i] == 101 || str[i] == 105 || str[i] == 111 || str[i] == 117)
-			{
-				*pcntrVovel++;				
-			}
-			else
-			{				
-				*pcntrConsonant++;
-			}
-		}
-						
-		else if (str[i] == 95) {
-			
-			*pcntrSp++;
-		} 		
-		
-		else if (str[i] == 33 || str[i] == 44){
-			*pcntrComa++;
-		}		
-	}	
-}
 
 
-void RemSymb(char *str, char rem) {
-	for (int i = 0; i < strlen(str); i++)
-	{
-		if (str[i] == rem) {
-			for (int j = 0; j < strlen(str); j++)
-			{
+							////===>>> TASK 2 <<<===////
+/*2) Розробити програму, яка зчитує з екрану рядок, а потім видає статистику: кількість
+пробільних символів (whitespaces), голосних, приголосних, знаків пунктуації.
+Введення передбачається англомовним.
+2)Rozrobyty programu, jaka zchytuje z ekranu rjadok, a potim vydaje statystyku: kilkist
+probilnyh symvoliv (whitespaces), golosnyh, prygolosnyh, znakiv punctuacii.
+Vvedennia peredbachaetcia anglomovnym.
+*/
 
-			}
-		}
-	}
-	
-
-}
-
-
-
-
+//////# include <iostream>
+//////# include <string>		//switch on class string
+//////using namespace std;
+//////
+////////Function whith parametres array str, pointers for the counters
+//////void SignsOfSound(char *str, int *pcntrVovel, int *pcntrConsonant, int *pcntrSp, int *pcntrComa);
+//////
+//////int main()
+//////{
+//////	int cauntVovel = 0;		 //golosni zvuky
+//////	int cauntConsonant = 0;	 //prygolosni zvuky
+//////	int cauntSp = 0;			 //counter of spaces
+//////	int cauntComa = 0;		 // counter of punctuation
+//////
+//////	int *pcauntVovel = &cauntVovel;
+//////	int *pcauntConsonant = &cauntConsonant;
+//////	int *pcauntSp = &cauntSp;
+//////	int *pcauntComa = &cauntComa;
+//////
+//////	char str[255];
+//////	cout << "==> This program caunts number of whitespaces, vovel, consonant sounds and punctuation signs <==" << endl;
+//////	cout << "Enter text in english: " << endl;
+//////	cin.getline(str, sizeof(str));		//Zamist cin>> - dozvoliae vrahovuvaty probily
+//////	
+//////	//This function counts number of whitespaces, vovel, consonant sounds and punctuation signs
+//////	
+//////	SignsOfSound(str, pcauntVovel, pcauntConsonant, pcauntSp, pcauntComa);
+//////
+//////	cout << "	Vovel sounds:	" << *pcauntVovel << endl;
+//////	cout << "	Consonant sounds:	" << *pcauntConsonant << endl;
+//////	cout << "	Punctuation signs:	" << *pcauntComa << endl;
+//////	cout << "	Whitespaces:	" << *pcauntSp << endl;
+//////	
+//////	system("pause");
+//////	return 0;
+//////}
+//////
+////////This function counts number of whitespaces, vovel, consonant sounds and punctuation signs
+//////void SignsOfSound(char *str, int *pcauntVovel, int *pcauntConsonant, int *pcauntSp, int *pcauntComa) {
+//////		
+//////	for (int i = 0; i < strlen(str); i++)
+//////	{
+//////		if (((int(str[i])) >= 65 && (int(str[i])) <= 90) || (int(str[i])) >= 97 && (int(str[i])) <= 122) 
+//////		{			
+//////			if ((int (str[i])) == 65 || (int (str[i])) == 69 || (int(str[i])) == 73 || (int(str[i])) == 79 || (int(str[i])) == 85 || (int(str[i])) == 97 || (int(str[i])) == 101 || (int(str[i])) == 105 || (int(str[i])) == 111 || (int(str[i])) == 117)
+//////			{
+//////				(*pcauntVovel)++;
+//////			}
+//////			else
+//////			{				
+//////				(*pcauntConsonant)++;
+//////			}
+//////		}
+//////						
+//////		else if ((int(str[i])) == 32) {
+//////			
+//////			(*pcauntSp)++;
+//////		} 		
+//////		
+//////		else if ((int(str[i])) == 33 || (int(str[i])) == 44){
+//////			(*pcauntComa)++;
+//////		}		
+//////	}	
+//////}
